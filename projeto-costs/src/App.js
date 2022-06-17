2da8f239417109ceeba160f3bc043ea2bffd0b43
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-06-17 10:40:00 -0400

    page project

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,28 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Container from './components/layout/Container/Container';
 import Company from './components/pages/Company/Company';
 import Contact from './components/pages/Contact/Contact';
 import Home from './components/pages/Home/Home';
 import NewProject from './components/pages/NewProject/NewProject';
+import Footer from './components/layout/Footer/Footer'
+import Navbar from './components/layout/Navbar/Navbar'
+import Projects from './components/pages/Projects/Projects';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/new-project">Novo Projeto</Link>
-        <Link to="/company">Empresa</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/company' element={<Company />} />
+          <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/new-project' element={<NewProject />} />
+          <Route exact path='/new-projects' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>footer</p>
+     <Footer />
     </Router>
   );
 }
